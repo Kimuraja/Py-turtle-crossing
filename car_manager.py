30951671247e210778d1025a0e10ceca37ce6071
Jakub Załęcki <jakub.zalecki@gmail.com>
2023-02-23 20:46:55 +0100

    Fixed isssue
    Fixed car generator speed

--- car_manager.py
+++ car_manager.py
@@ -7,23 +7,23 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
+# TODO, Slow down cars !
+
 class CarManager(Turtle):
     def __init__(self):
         super().__init__()
         self.segment = []
 
     def new_car(self):
-        new = Turtle()
+        new = Turtle("square")
         new.left(180)
         new.pu()
-        new.shape("square")
         new.shapesize(stretch_wid=1, stretch_len=2)
         new.color(random.choice(COLORS))
-        new.goto(300, random.randint(-260, 260))
+        new.goto(320, random.randint(-250, 250))
+        # for _ in range(len(COLORS)):
         self.segment.append(new)
 
     def move(self):
-        for i in range(0, len(self.segment)):
+        for i in range(0, len(self.segment), 5):
             self.segment[i].fd(STARTING_MOVE_DISTANCE)
-
-
