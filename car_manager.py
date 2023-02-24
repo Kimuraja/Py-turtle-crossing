020aa0dd084fda2db919133c45d119fab88b2701
Jakub Załęcki <jakub.zalecki@gmail.com>
2023-02-24 20:10:54 +0100

    Issues fixed

--- car_manager.py
+++ car_manager.py
@@ -1,31 +1,27 @@
-import random
 from turtle import Turtle
-import time
+import random
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
-
+STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager(Turtle):
+class CarManager:
+
     def __init__(self):
-        super().__init__()
-        self.shape("square")
-        self.STARTING_MOVE_DISTANCE = 5
-        self.pu()
-        self.shapesize(stretch_wid=1, stretch_len=2)
-        self.color(random.choice(COLORS))
-        self.left(180)
-        self.goto(x=280, y=random.randint(-280, 280))
+        self.all_cars = []
 
-    def generate_car(self):
-        i = Turtle("square")
-        i.STARTING_MOVE_DISTANCE = 5
-        i.pu()
-        i.shapesize(stretch_wid=1, stretch_len=2)
-        i.color(random.choice(COLORS))
-        i.left(180)
-        i.goto(x=280, y=random.randint(-280, 280))
+    def create_car(self):
+        rand_chance = random.randint(1, 6)
+        if rand_chance == 1:
+            i = Turtle("square")
+            i.pu()
+            i.shapesize(stretch_wid=1, stretch_len=2)
+            i.color(random.choice(COLORS))
+            i.left(180)
+            i.goto(x=280, y=random.randint(-250, 250))
+            self.all_cars.append(i)
 
-    def move(self):
-        self.fd(self.STARTING_MOVE_DISTANCE)
+    def move_car(self):
+        for i in range(0, len(self.all_cars)):
+            self.all_cars[i].fd(STARTING_MOVE_DISTANCE)
--- main.py
+++ main.py
@@ -4,33 +4,38 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
-score = Scoreboard()
-p = Player()
+
 s = Screen()
 s.setup(width=600, height=600)
 s.tracer(0)
 s.bgcolor("#edf2f4")
+
 car = CarManager()
+
+p = Player()
 p.starting_position()
-# new = car.add_segment()
+
 s.listen()
 s.onkeypress(p.move, "Up")
+
+score = Scoreboard()
 
 game_is_on = True
 while game_is_on:
     s.update()
-    car.generate_car()
     time.sleep(0.1)
     score.update_scoreboard()
-    car.move()
+
+    car.create_car()
+    car.move_car()
 
     if p.ycor() == 290:
         p.reset_game()
-        car.STARTING_MOVE_DISTANCE += 10
         score.increase_score()
 
-    if car.distance(p) < 20:
-        score.game_over()
-        game_is_on = False
+    for car in car.all_cars:
+        if car.distance(p) < 20:
+            score.game_over()
+            game_is_on = False
 
 s.exitonclick()
